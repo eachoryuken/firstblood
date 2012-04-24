a2be44ba2b3b712464a342b8fb46a27016308a97
Marco Salles <marcosalles@gmail.com>
2012-04-24 17:48:49 -0300

    {adicionei comandos de toque no jogador}

--- Assets/Scripts/PCs/Lulz.cs
+++ Assets/Scripts/PCs/Lulz.cs
@@ -4,13 +4,21 @@ using System.Collections;
 public class Lulz : MonoBehaviour {
 
 	void Update () {
+		float zpeed = 0.2f;
 		float z=transform.position.z;
 		float x=transform.position.x;
 	
-		if((Input.GetKey("w"))||Input.GetKey("up")){z+=0.2f;}
-		if((Input.GetKey("s"))||Input.GetKey("down")){z-=0.2f;}
+		if((Input.GetKey("w"))||Input.GetKey("up")){z+=zpeed;}
+		if((Input.GetKey("s"))||Input.GetKey("down")){z-=zpeed;}
 		if((Input.GetKey("a"))||Input.GetKey("left")){x-=0.2f;}
 		if((Input.GetKey("d"))||Input.GetKey("right")){x+=0.2f;}
+		
+		if (Input.touchCount == 1) {
+			z+=zpeed;
+		}
+		if (Input.touchCount == 2) {
+			x+=zpeed;
+		}
 		
 		transform.position=new Vector3(x,transform.position.y,z);
 		GlobalVars.herox=transform.position.x;
