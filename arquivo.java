aed7f912c2934c79ec5deaeab16cc1f1070716a7
unknown <MagonX@AUTOBOT.(none)>
2012-05-10 16:46:15 -0300

    screwed the file on line 5

--- arquivo.java
+++ arquivo.java
@@ -3,8 +3,9 @@ arquivo
 
 public static void main (String[] args) {
 
-System.out.println("Texto");
+System.out.println("Textoasdsadsadsdas");
 System.out.println("Oi");
+aydfosdihoidaugpoaeouo
 
 
 }
